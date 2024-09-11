01e8d2aafeb9da1d10aa9b242de9ff24bdb9b393
DenzelTorrico <denzel_silva@hotmail.com>
2024-09-10 22:02:47 -0500

    index

---
+++ src/views/AboutView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-table :data="paginatedData" style="width: 100%;">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="pageSize"
+      :current-page="currentPage"
+      @current-change="handlePageChange"
+    />
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+    const tableData = ref([])
+
+    const currentPage = ref(1)
+    const pageSize = ref(10) // Número de filas por página
+    const total = computed(() => tableData.value.length)
+
+    const paginatedData = computed(() => {
+      const start = (currentPage.value - 1) * pageSize.value
+      const end = start + pageSize.value
+      return tableData.value.slice(start, end)
+    })
+
+    const handlePageChange = (page) => {
+      currentPage.value = page
+    }
+
+
+ 
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Libros disponibles:</h2>
+  
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+// Definir una referencia reactiva para almacenar los libros
+const vehicules = ref([]);
+const baseUrl = import.meta.env.VITE_API_LUMEN;
+// Función para cargar los datos de la API
+const loadData = async () => {
+  try {
+   const response = await axios.get(`${baseUrl}/vehicules`);
+   console.log(response.data); // Mostrar los libros en la consola
+  } catch (error) {
+    console.error("Error al cargar los libros:", error);
+  }
+};
+
+// Ejecutar la función cuando el componente esté montado
+onMounted(() => {
+  loadData();
+});
+</script>
