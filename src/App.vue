903701c9f666918ac729a499bcd012124ed83017
DenzelTorrico <denzel_silva@hotmail.com>
2024-09-10 22:44:10 -0500

    compilados

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-
-  
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">Acerca de</RouterLink>
       </nav>
    
   </header>
